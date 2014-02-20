4f9a94fc0073d271ab207277d9bdd0d199f85115
Darren Cauthon <darren@cauthon.com>
2014-02-20 12:11:59 -0600

    Initial commit.

---
+++ lib/starting_blocks/stopplicht.rb
@@ -0,0 +1,7 @@
+require "starting_blocks/stopplicht/version"
+
+module StartingBlocks
+  module Stopplicht
+    # Your code goes here...
+  end
+end
---
+++ lib/starting_blocks/stopplicht/version.rb
@@ -0,0 +1,5 @@
+module StartingBlocks
+  module Stopplicht
+    VERSION = "0.0.1"
+  end
+end
