499f645f1a1326c48d12fdc5a1f8331a26d30263
Darren Cauthon <darren@cauthon.com>
2014-05-23 23:40:57 -0500

    Finish testing the receive results method.

--- spec/stopplicht_alert_spec.rb
+++ spec/stopplicht_alert_spec.rb
@@ -5,14 +5,30 @@ require 'mocha/setup'
 
 describe 'stopplicht' do
 
+  before do
+  end
+
   describe "receiving test results" do
 
     it "should display the results color" do
+
+      StartingBlocks::Extensions::StopplichtAlert
+        .any_instance.stubs(:display)
+
       color = Object.new
       alert = StartingBlocks::Extensions::StopplichtAlert.new
-      alert.expects(:display).with color
 
+      alert.receive_files_to_run ["test.txt"]
+
+      alert.expects(:display).with color
       alert.receive_results( { color: color } )
+    end
+
+    it "should not display any results if no files were passed" do
+      alert = StartingBlocks::Extensions::StopplichtAlert.new
+      alert.receive_files_to_run []
+      alert.expects(:display).never
+      alert.receive_results( { color: Object.new } )
     end
 
   end
